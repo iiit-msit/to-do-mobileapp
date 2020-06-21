3ec434555f2fff665cafcfe4af1d585b6d9690b1
Visvanath27 <visvanath.kanigiri@msitprogram.net>
2020-06-21 20:51:28 +0530

    with sign in refresh token generation

--- todo/App.js
+++ todo/App.js
@@ -6,7 +6,7 @@ import { Header } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import moment from 'moment';
 import AccordianMenu from "./AccordionMenu";
-import {AsyncStorage} from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 
 
@@ -22,41 +22,81 @@ export default class App extends React.Component {
       date: moment(today).format("YYYY-MM-DD"),
       invalidUser: false,
       loading: true,
+      idToken: ""
     }
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('userDetails').then((details) => {
+      var obj = JSON.parse(details);
+      if (obj) {
+        this.state.signedIn = true;
+        this.get_events()
+        this.setState({
+          signedIn: true,
+          email: obj.user.email,
+          access: obj.accessToken,
+          refreshToken: obj.refreshToken,
+          idToken: obj.idToken
+        })
+      }
+    });
   }
 
   signIn = async () => {
     try {
-      this.setState({loading:true})
-      const result = await Google.logInAsync({
-        androidClientId:
-          "957732613139-a4rbsd7po036hqh8u6ocn9vslsbgn9m8.apps.googleusercontent.com",
-
-      })
-      if (result.type == "success") {
+      this.setState({ loading: true })
+      let resp = await AsyncStorage.getItem('userDetails')
+      if (resp != null) {
+        let userDetails = resp;
+        console.log(userDetails)
+        let info = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${userDetails.idToken}`)
+          .then(response => {
+            if (response.status != 200) {
+              fetch(`https://oauth2.googleapis.com/token?client_id=957732613139-a4rbsd7po036hqh8u6ocn9vslsbgn9m8.apps.googleusercontent.com&refresh_token=${userDetails.refreshToken}&grant_type=refresh_token`,
+                { method: 'post' })
+                .then((response) => response.status == 200 ? response.json() : {})
+            }
+          })
         this.setState({
           signedIn: true,
-          email: result.user.email,
-          access: result.accessToken,
+          email: userDetails.user.email,
+          access: info.accessToken,
+          refreshToken: userDetails.refreshToken,
+          idToken: userDetails.idToken
         })
-       // console.log(result);
-        const userDetails = await result.json;
-        AsyncStorage.setItem('userData',JSON.stringify(userDetails));
-        this.get_events()
-        
-        
-        
-       
+        userDetails.accessToken = info.accessToken;
+        await AsyncStorage.setItem('userDetails', JSON.stringify(userDetails));
 
+      } else {
+        const result = await Google.logInAsync({
+          androidClientId:
+            "957732613139-a4rbsd7po036hqh8u6ocn9vslsbgn9m8.apps.googleusercontent.com",
+        })
+
+        if (result.type == "success") {
+          this.setState({
+            signedIn: true,
+            email: result.user.email,
+            access: result.accessToken,
+            refreshToken: result.refreshToken,
+            idToken: result.idToken
+          })
+          // console.log(result);
+          const userDetails = await result.json;
+          AsyncStorage.setItem('userDetails', JSON.stringify(userDetails));
+        }
       }
+      this.get_events()
     } catch (e) {
       console.log("error", e)
     }
   }
   async get_events() {
+    await this.checkDetails();
     //console.log(this.state.email);
     //console.log(this.state.date);
-    await fetch("http://192.168.43.132:5000/mydayevents" + "/" + this.state.email + "/" + this.state.date)
+    await fetch(`http://192.168.43.132:5000/mydayevents/${this.state.email}/${this.state.date}`)
       .then(response => response.json())
       .then((data) => {
 
@@ -65,16 +105,47 @@ export default class App extends React.Component {
 
           dataSource: data
         })
-
-
       })
       .catch(error => console.log(error))
   }
-  signout(){
-    this.setState({signedIn:false}),
-    AsyncStorage.clear();    
-    <LoginPage signIn={this.signIn}/>
+  signout = async () => {
+    this.setState({ signedIn: false }),
+      await AsyncStorage.removeItem("userDetails");
+    <LoginPage signIn={this.signIn} />
   }
+
+  // checkDetails = () => {
+
+  //   // console.debug(`Trying to Check url for token ${this.state.idToken}`)
+  //   // await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${this.state.idToken}`)
+  //   //   .then(
+  //   //     (response) => {
+  //   //       if (response.status == 200) {
+  //   //         response.json((data) => {
+  //   //           console.log(data)
+  //   //         })
+  //   //       }
+  //   //     });
+  //   console.debug(`Trying to get from refresh token ${this.state.refreshToken}`)
+  //   fetch(`https://oauth2.googleapis.com/token?client_id=957732613139-a4rbsd7po036hqh8u6ocn9vslsbgn9m8.apps.googleusercontent.com&refresh_token=${this.state.refreshToken}&grant_type=refresh_token`,
+  //     { method: 'post' })
+  //     .then((response) => response.status == 200 ? response.json() : {})
+  //     .then(json => {
+  //       console.log(json)
+  //     })
+  // }
+
+
+  //new method
+  // async revoke_Access() {
+  //   await fetch("https://oauth2.googleapis.com/revoke", {
+  //     method: 'POST',
+  //     params: { 'token': result.accessToken },
+  //     headers: { 'content-type': 'application/x-www-form-urlencoded' }
+  //   });
+  // }
+
+
 
   render() {
     return (
@@ -82,47 +153,47 @@ export default class App extends React.Component {
         {this.state.signedIn ? (
 
           <View style={styles.container}>
-           <Header
-           
-           leftComponent={
-             <DatePicker
-               date={this.state.date}
-               mode="date"
-               placeholder="select date"
-               format="YYYY-MM-DD"
-               minDate="2019-05-01"
-               maxDate="2021-06-01"
-               confirmBtnText="Confirm"
-               cancelBtnText="Cancel"
-               customStyles={{
-                 dateIcon: {
-                   position: 'absolute',
-                   left: 0,
-                   bottom : 20,
-                   marginLeft: 0,  
-                 },
-                 dateInput: {
-                   marginLeft:30,
-                   marginBottom: 30,
-                 
-                 }
-               }}
-               onDateChange={(date) => {this.setState({date: date}),this.get_events()}}     
-             />}
-           centerComponent={{ text: 'ToDo List', style: { color: 'white',fontSize : 30 ,fontWeight:"bold",marginBottom:30,textAlign:"center",marginLeft: 40} }}
-           rightComponent={
-             <View style = {{marginBottom:30,size : 100}}>
-             <Button
-               color = "green"
-               title="LogOut"
-               
-               onPress={() => this.signout()
-             }/>
-           </View>
-       }
-           
-           backgroundColor="#3D6DCC"
-         />
+            <Header
+
+              leftComponent={
+                <DatePicker
+                  date={this.state.date}
+                  mode="date"
+                  placeholder="select date"
+                  format="YYYY-MM-DD"
+                  minDate="2019-05-01"
+                  maxDate="2021-06-01"
+                  confirmBtnText="Confirm"
+                  cancelBtnText="Cancel"
+                  customStyles={{
+                    dateIcon: {
+                      position: 'absolute',
+                      left: 0,
+                      bottom: 20,
+                      marginLeft: 0,
+                    },
+                    dateInput: {
+                      marginLeft: 30,
+                      marginBottom: 30,
+
+                    }
+                  }}
+                  onDateChange={(date) => { this.setState({ date: date }), this.get_events() }}
+                />}
+              centerComponent={{ text: 'ToDo List', style: { color: 'white', fontSize: 30, fontWeight: "bold", marginBottom: 30, textAlign: "center", marginLeft: 40 } }}
+              rightComponent={
+                <View style={{ marginBottom: 30, size: 100 }}>
+                  <Button
+                    color="green"
+                    title="LogOut"
+
+                    onPress={() => this.signout()
+                    } />
+                </View>
+              }
+
+              backgroundColor="#3D6DCC"
+            />
             {this.state.dataSource.length === 0 ? (
               <View style={styles.container}>
                 <Text style={{ fontSize: 30 }}>No event scheduled today</Text>
@@ -155,18 +226,18 @@ export default class App extends React.Component {
         <AccordianMenu
           title={item.summary}
           data={(<Text>Description :{item.description}</Text>)}
-          created_on = {(<Text>Created on :{item.created}</Text>)} 
-          Start = {(<Text>Start at :{item.start.dateTime}</Text>)}
-          End = {(<Text>Ends at :{item.end.dateTime}</Text>)}
-          Created_by = {(<Text>Created by: {item.creator.email}</Text>)}
-          
-          
-          
-          
-          
-//     <Text style={styles.text}>Start : {item.start.dateTime} </Text>
-//     <Text style={styles.text}>End : {item.end.dateTime} </Text>
-//     <Text style={styles.text}>Created By : {item.creator.email} </Text>
+          created_on={(<Text>Created on :{item.created}</Text>)}
+          Start={(<Text>Start at :{item.start.dateTime}</Text>)}
+          End={(<Text>Ends at :{item.end.dateTime}</Text>)}
+          Created_by={(<Text>Created by: {item.creator.email}</Text>)}
+
+
+
+
+
+        //     <Text style={styles.text}>Start : {item.start.dateTime} </Text>
+        //     <Text style={styles.text}>End : {item.end.dateTime} </Text>
+        //     <Text style={styles.text}>Created By : {item.creator.email} </Text>
 
 
         />
